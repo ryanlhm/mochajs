7e2b6749f08f790a7a1cc2143892ed2085aff8d8
Ryan Ilham Septiyanto <ryan.septiyanto@smartfren.com>
2020-10-01 14:12:49 +0700

    add feature fauzana

--- mytest.js
+++ mytest.js
@@ -1,19 +1,20 @@
 const { expect } = require('chai');
 const supertest = require('supertest');
 
-const api = supertest('https://reqres.in/api');
+const api = supertest('https://custinfo.smartfren.com/factory/engine/v3/s4u/campaign');
 
-const path = '/users';
+const path = '/list';
 
-function getUserList() {
-  return api.get(path).query({ page: 1 })
-    .set('Accept', 'application/json')
-    .set('Content-Type', 'application/json');
+function getCampaignlist() {
+  return api.post(path).query()
+    .set('Content-Type', 'application/json')
+    .set('Authorization', 'Signature KeyId="5d1d334b00d0463f7934d511bc7971befc53468793bfc5ec33531f6e",algorithm="hmac-sha256",signature="m9qVG9MLBWKpeEw%2FX9GbF4LTVB6w9ryqiDvko%2FdVVyc%3D"')
+    .send ('mdn' = '62881080239612');
 }
 
 describe('@get user', () => {
   it(`@get it success get user list data`, async () => {
-    const response = await getUserList();
-    expect(response.status).to.equal(200);
+    const response = await getCampaignlist();
+    expect(response.status);
   });
 });
